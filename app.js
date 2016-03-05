b110dc5f104f7a1f18cd5c728fdcb2632774eff7
Carlin Au <carlinau@gmail.com>
2016-03-05 15:03:21 -0500

    Basic routes and stuff
    basic stuff

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+/* var Botkit = require('botkit');
+var controller = Botkit.slackbot();
+var bot = controller.spawn({
+  // token: my_slack_bot_token
+})
+bot.startRTM(function(err, bot, payload){
+  if(err){
+    throw new Error('Could not connect to Slack');
+  }
+});
+
+controller.hears(["keyword","^pattern$"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+  // do something to respond to message
+  // all of the fields available in a normal Slack message object are available
+  // https://api.slack.com/events/message
+  bot.reply(message,'You used a keyword!');
+});
+*/
+
+app.get('/', function(req,res) {
+  res.sendFile('/index.html');
+});
+
+app.listen(8080);
