f9350020b334970df24ddb69aa82a8f2a4f1819b
Carlin Au <carlinau@gmail.com>
2016-03-05 23:33:54 -0500

    shit works yo, i think

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var port = process.env.port || 8080;
+var config = require('./config');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+var request = require('request');
+var redirect_uri_spotify = 'http://localhost:8080/callback-spotify';
+
+app.use(cookieParser());
+
 /* var Botkit = require('botkit');
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
@@ -21,8 +29,127 @@ controller.hears(["keyword","^pattern$"],["direct_message","direct_mention","men
 });
 */
 
+var stateKey = 'spotify_auth_state';
+
 app.get('/', function(req,res) {
+  console.log(config.spotify_id);
+  console.log(config.spotify_secret);
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/login', function(req, res) {
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  var scope = 'playlist-modify-public playlist-modify-private';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: config.spotify_id,
+      scope: scope,
+      redirect_uri: redirect_uri_spotify,
+      state: state
+    }));
+});
+
+app.get('/callback-spotify', function(req, res) {
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+    querystring.stringify({
+      error: 'state_mismatch'
+    }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri_spotify,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(config.spotify_id + ':' + config.spotify_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+        refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+        querystring.stringify({
+          access_token: access_token,
+          refresh_token: refresh_token
+        }));
+      } else {
+        res.redirect('/#' +
+        querystring.stringify({
+          error: 'invalid_token'
+        }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(config.spotify_id + ':' + config.spotify_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
 app.listen(port);
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
